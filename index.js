eed564de3353b0a25af48052bab0788b56b6cd7a
suey0128 <syui0128@gmail.com>
2021-05-20 12:36:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+
+function findMatching(driverArray, name) {
+    return driverArray.filter(function (element) {return element.toUpperCase() === name.toUpperCase()})
+};
+   
+function fuzzyMatch (driverArray, startingLetter) {
+    // console.log(driverArray, name);
+    return driverArray.filter(function(element) {return element.startsWith(startingLetter)});
+ }
+
+function matchName (driverObj, driverName) {
+    // console.log(driverObj, name);
+    for (const element in driverObj) {
+        return driverObj.filter(function(element) {return element.name === driverName});
+    }
+}
